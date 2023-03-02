6b7d3408330141b051138a634b31f1fa4d952631
Sergei Fedotov <fsa@tavda.info>
2023-03-03 04:50:10 +0700

    Добавлен twig. Cоздан User и миграция. Тестовый путь в root контроллере

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class Controller extends AbstractController
 {
@@ -12,5 +13,12 @@ class Controller extends AbstractController
     public function index(): Response
     {
         return new Response('Welcome to your new controller!');
+    }
+
+    #[Route('/test', name: 'test')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function test(): Response
+    {
+        return new Response('Test!');
     }
 }
