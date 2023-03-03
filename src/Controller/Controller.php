d27e7d4cf81cb496df4d9eebe219740aa1e5f423
Sergei Fedotov <fsa@tavda.info>
2023-03-03 18:51:17 +0700

    Выход и шаблоны

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -6,19 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Twig\Environment;
 
 class Controller extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Environment $twig): Response
     {
-        return new Response('Welcome to your new controller!');
+        return new Response($twig->render('index.html.twig', ['message'=> 'Welcome to your new controller!
+']));
     }
 
     #[Route('/test', name: 'test')]
     #[IsGranted('ROLE_ADMIN')]
-    public function test(): Response
+    public function test(Environment $twig): Response
     {
-        return new Response('Test!');
+        return new Response($twig->render('index.html.twig', ['message' => 'Test!
+']));
     }
 }
